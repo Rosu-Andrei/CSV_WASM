6a515df529b107fd9a393bd73a12092374172594
Rosu Andrei <andrei_rosu1986@yahoo.com>
2024-10-16 15:57:31 +0300

    master : the csv file is successfully parsed using the parse_csv method from the .wasm file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
+// src/app/app.module.ts
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+// Angular Material Modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { CsvParserComponent } from './components/csv-parser/csv-parser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CsvParserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule
   ],
-  providers: [
-    provideClientHydration()
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
